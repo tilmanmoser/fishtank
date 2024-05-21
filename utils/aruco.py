09a026f78a2636d18c0a8d02503158ea6da6378a
Tilman Moser <tilman.moser@impavit.com>
2024-05-21 16:08:08 +0200

    Helper script for generating aruco markers

---
+++ utils/aruco.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+import cv2
+
+
+def generate_aruco_markers(dictionary_type=cv2.aruco.DICT_4X4_1000, amount=16, size=200):
+    dictionary = cv2.aruco.getPredefinedDictionary(dictionary_type)
+    markers = []
+    for i in range(amount):
+        marker = cv2.aruco.generateImageMarker(dictionary=dictionary, id=i, sidePixels=size)
+        markers.append(marker)
+    return markers
+
+
+def main():
+    Path(os.path.join("./data/aruco")).mkdir(parents=True, exist_ok=True)
+    markers = generate_aruco_markers()
+    for i, marker in enumerate(markers):
+        cv2.imwrite(os.path.join("./data/aruco", f"aurco-{i}.png"), marker)
+
+
+if __name__ == "__main__":
+    main()
