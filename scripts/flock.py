8cde3c1423a30371cd521f65999606552d6e81b3
Tilman Moser <tilman.moser@impavit.com>
2024-05-21 11:43:11 +0200

    Refactoring

--- scripts/flock.py
+++ scripts/flock.py
@@ -5,42 +5,35 @@ import pygame
 
 class Flock:
     def __init__(self, max=25, area=(0, 0, 0, 0)) -> None:
+        # limits
         self.max_boids = max
         self.min_position = np.array(area[:2])
         self.max_position = np.array(area[2:])
-
         self.min_velocity = np.array([0, -20])
         self.max_velocity = np.array([10, 20])
 
+        # boids
         self.positions = np.empty((0, 2), float)
         self.velocities = np.empty((0, 2), float)
 
     def add(self):
         if len(self.positions) >= self.max_boids:
+            # remove first(=oldest) element from array
             self.positions = np.delete(self.positions, 1, 0)
             self.velocities = np.delete(self.velocities, 1, 0)
 
-        self.positions = np.append(
-            self.positions,
-            (
-                self.min_position[:, np.newaxis]
-                + np.random.rand(2, 1)
-                * (self.max_position - self.min_position)[:, np.newaxis]
-            ).reshape(1, 2),
-            axis=0,
-        )
+        # append new boid
+        self.positions = np.append(self.positions, self.random2d(self.min_position, self.max_position), axis=0)
+        self.velocities = np.append(self.velocities, self.random2d(self.min_velocity, self.max_velocity), axis=0)
 
-        self.velocities = np.append(
-            self.velocities,
-            (
-                self.min_velocity[:, np.newaxis]
-                + np.random.rand(2, 1)
-                * (self.max_velocity - self.min_velocity)[:, np.newaxis]
-            ).reshape(1, 2),
-            axis=0,
-        )
+    def random2d(self, lower_limits=np.array([0, 0]), upper_limits=np.array([0, 0])):
+        range = upper_limits - lower_limits
+        rand = lower_limits[:, np.newaxis] + np.random.rand(2, 1) * range[:, np.newaxis]
+        rand = rand.reshape(1, 2)
+        return rand
 
     def update(self, timestep):
+        # boid movement
         self.positions += timestep * self.velocities
 
     def render(self, surface):
