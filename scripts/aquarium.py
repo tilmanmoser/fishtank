40d207ac3550a839d5423fb72b4619c536611504
Tilman Moser <tilman.moser@impavit.com>
2024-05-23 11:26:29 +0200

    capture fishes from webcam

--- scripts/aquarium.py
+++ scripts/aquarium.py
@@ -5,6 +5,7 @@ import cv2
 import pygame
 
 from watchdog.observers import Observer
+from scripts.capture import Capture
 from scripts.flock import Flock
 from scripts.scanner import Scanner
 from scripts.imageFileEventHandler import ImageFileEventHandler
@@ -14,99 +15,43 @@ FPS = 25  # adjust to video FPS
 
 class Aquarium:
     def __init__(self) -> None:
-        # display
+        # setup display
         pygame.init()
-        pygame.display.set_caption("Flock")
+        self.display = pygame.Surface((1280, 720))
         self.scale = 1
         self.margin = [0, 0]
-        self.display = pygame.Surface((1280, 720))
+        pygame.display.set_caption("Flock")
         self.screen = pygame.display.set_mode(self.display.get_size(), pygame.RESIZABLE)
         self.on_resize()
-
-        # background video & audio
-        self.video = cv2.VideoCapture(os.path.join("data/video/underwater.mp4"))
-        self.audio = pygame.mixer.Sound(os.path.join("data/audio/aquarium-ambience.mp3"))
-        self.muted = True
-
-        # objects
-        self.movement = [False, False, False, False]
-        self.flock = Flock(max=25, area=(100, 100, self.display.get_width() - 100, self.display.get_height() - 100))
-        self.load_boids()
-
-        # ticks / fps
-        self.clock = pygame.Clock()
 
         # hide mouse
         pygame.mouse.set_visible(False)
 
-    def load_boids(self):
-        path = os.path.join("data/outbound")
-        # load 25 boids reverse sorted by date, but in sorted order, so that the oldest is at index 0
-        for file in sorted(sorted(os.listdir(path), reverse=True)[:25]):
-            self.load_boid(os.path.join(path, file))
+        # ticks / fps
+        self.clock = pygame.Clock()
 
-    def load_boid(self, file):
-        match = re.search("^.*[0-9]{20}-([0-9]{1}).png", file)
-        if match:
-            # load image
-            img = pygame.image.load(os.path.join(file)).convert_alpha()
-            # scale down
-            img = pygame.transform.smoothscale(img, (100, 142))
-            # make them look "right" (which is 0 degree angle in pygame)
-            img = pygame.transform.flip(pygame.transform.rotate(img, -90), False, True)
-            # add fish to flock, match.group(1) == type of fisch from filename
-            self.flock.add(img)
+        # background video & audio
+        self.video = cv2.VideoCapture(os.path.join("data/video/underwater.mp4"))
+        self.audio = pygame.mixer.Sound(os.path.join("data/audio/aquarium-ambience.mp3"))
+        self.muted = False
 
-    def on_resize(self):
-        self.scale = min(
-            self.screen.get_width() / self.display.get_width(), self.screen.get_height() / self.display.get_height()
-        )
-        self.margin[0] = int((self.screen.get_width() - self.display.get_width() * self.scale) // 2)
-        self.margin[1] = int((self.screen.get_height() - self.display.get_height() * self.scale) // 2)
-
-    def update(self):
-        self.flock.update(
-            self.clock.get_time() / 1000, (self.movement[1] - self.movement[0], self.movement[3] - self.movement[2])
+        # fish importing
+        inbound = os.path.join("data", "inbound")
+        outbound = os.path.join("data", "outbound")
+        scanner = Scanner()
+        self.observer = Observer()
+        self.observer.schedule(ImageFileEventHandler(scanner, outbound, True, self.load_boid), inbound, recursive=True)
+        self.capture = Capture(
+            scanner, outbound, size=(640, 480), pos=(self.display.get_width() / 2 - 320, 0), callback=self.load_boid
         )
 
-    def render(self):
-        self.display.fill((0, 0, 0, 0))
-        self.display.blit(self.get_video_frame(), (0, 0))
-        self.flock.render(self.display)
-
-        self.screen.fill((0, 0, 0, 0))
-        self.screen.blit(pygame.transform.smoothscale_by(self.display, self.scale), self.margin)
-        pygame.display.flip()
-
-    def get_video_frame(self):
-        if not self.video.isOpened():
-            return pygame.Surface(self.display.get_size())
-        success, frame = self.video.read()
-        if success:
-            surface = pygame.image.frombuffer(frame.tobytes(), frame.shape[1::-1], "BGR")
-            return surface
-        else:
-            self.video.set(cv2.CAP_PROP_POS_FRAMES, 0)
-            return self.get_video_frame()
-
-    def toggle_audio(self):
-        self.muted = not self.muted
-        if self.muted:
-            self.audio.stop()
-        else:
-            self.audio.set_volume(1.0)
-            self.audio.play(-1)
+        # objects
+        self.movement = [False, False, False, False]
+        self.flock = Flock(max=25, area=(100, 100, self.display.get_width() - 100, self.display.get_height() - 100))
+        self.load_boids(outbound)
 
     def run(self):
-        # file observer for adding boids
-        observer = Observer()
-        image_event_handler = ImageFileEventHandler(
-            Scanner(), os.path.join("data", "outbound"), remove=True, callback=self.load_boid
-        )
-        observer.schedule(image_event_handler, os.path.join("data", "inbound"), recursive=True)
-        observer.start()
-
-        # audio
+        self.observer.start()
         self.toggle_audio()
 
         # main loop
@@ -135,6 +80,8 @@ class Aquarium:
                             self.flock.feed()
                         if event.key == pygame.K_m:
                             self.toggle_audio()
+                        if event.key == pygame.K_c:
+                            self.toggle_capture()
                     if event.type == pygame.KEYUP:
                         if event.key in (pygame.K_LEFT, pygame.K_a):
                             self.movement[0] = False
@@ -148,7 +95,79 @@ class Aquarium:
                 self.clock.tick(FPS)
         finally:
             # clean up
-            observer.stop()
-            observer.join()
-            self.video.release()
-            pygame.quit()
+            self.stop()
+
+    def stop(self):
+        self.observer.stop()
+        self.observer.join()
+        self.video.release()
+        self.capture.close()
+        pygame.quit()
+
+    def load_boids(self, path):
+        # load 25 boids reverse sorted by date, but in sorted order, so that the oldest is at index 0
+        for file in sorted(sorted(os.listdir(path), reverse=True)[:25]):
+            self.load_boid(os.path.join(path, file))
+
+    def load_boid(self, file):
+        match = re.search("^.*[0-9]{20}-([0-9]{1}).png", file)
+        if match:
+            # load image
+            img = pygame.image.load(os.path.join(file)).convert_alpha()
+            # scale down
+            img = pygame.transform.smoothscale(img, (100, 142))
+            # make them look "right" (which is 0 degree angle in pygame)
+            img = pygame.transform.flip(pygame.transform.rotate(img, -90), False, True)
+            # add fish to flock, match.group(1) == type of fisch from filename
+            self.flock.add(img)
+
+    def on_resize(self):
+        self.scale = min(
+            self.screen.get_width() / self.display.get_width(), self.screen.get_height() / self.display.get_height()
+        )
+        self.margin[0] = int((self.screen.get_width() - self.display.get_width() * self.scale) // 2)
+        self.margin[1] = int((self.screen.get_height() - self.display.get_height() * self.scale) // 2)
+
+    def update(self):
+        self.flock.update(
+            self.clock.get_time() / 1000, (self.movement[1] - self.movement[0], self.movement[3] - self.movement[2])
+        )
+        if self.capture.isCapturing():
+            self.capture.update()
+
+    def render(self):
+        self.display.fill((0, 0, 0, 0))
+        self.display.blit(self.get_video_frame(), (0, 0))
+        self.flock.render(self.display)
+
+        if self.capture.isCapturing():
+            self.capture.render(self.display)
+
+        self.screen.fill((0, 0, 0, 0))
+        self.screen.blit(pygame.transform.smoothscale_by(self.display, self.scale), self.margin)
+        pygame.display.flip()
+
+    def get_video_frame(self):
+        if not self.video.isOpened():
+            return pygame.Surface(self.display.get_size())
+        success, frame = self.video.read()
+        if success:
+            surface = pygame.image.frombuffer(frame.tobytes(), frame.shape[1::-1], "BGR")
+            return surface
+        else:
+            self.video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            return self.get_video_frame()
+
+    def toggle_audio(self):
+        self.muted = not self.muted
+        if self.muted:
+            self.audio.stop()
+        else:
+            self.audio.set_volume(1.0)
+            self.audio.play(-1)
+
+    def toggle_capture(self):
+        if self.capture.isCapturing():
+            self.capture.close()
+        else:
+            self.capture.open()
---
+++ scripts/capture.py
@@ -0,0 +1,123 @@
+import math
+import os
+import cv2
+import numpy as np
+import pygame
+import time
+from datetime import datetime
+
+
+class Capture:
+
+    def __init__(self, scanner, outbound, size=(320, 240), pos=(0, 0), callback=None) -> None:
+        self.scanner = scanner
+        self.outbound = outbound
+        self.temp = os.path.join("data/temp")
+        self.size = size
+        self.pos = pos
+        self.callback = callback
+
+        self.capture = None
+        self.last_frame_ts = 0
+        self.frame = pygame.Surface(size)
+        self.frame.fill((0, 0, 0))
+
+        self.aruco_detector = cv2.aruco.ArucoDetector(
+            dictionary=cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+        )
+
+        self.instructions = pygame.image.load(os.path.join("data/images/capture/instructions.png")).convert_alpha()
+        self.closer = pygame.image.load(os.path.join("data/images/capture/closer.png")).convert_alpha()
+
+    def open(self):
+        try:
+            self.capture = cv2.VideoCapture(0)
+        except Exception:
+            pass
+
+    def close(self):
+        try:
+            self.capture.close()
+        except Exception:
+            pass
+        finally:
+            self.capture = None
+
+    def isCapturing(self):
+        return isinstance(self.capture, cv2.VideoCapture) and self.capture.isOpened()
+
+    def update(self):
+        if self.isCapturing() and time.time() - self.last_frame_ts > (1.0 / 25):
+            success, frame = self.capture.read()
+            if success:
+                # hints
+                show_instructions = False
+                show_come_closer = False
+
+                # get frame dimensions
+                height, width, channels = frame.shape
+                diagonal = math.hypot(height, width)
+
+                # convert to gray color
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                # detect aruco markers
+                (markers, ids, rejected) = self.aruco_detector.detectMarkers(gray)
+
+                # if all expected markers are detected in frame
+                if len(markers) == 5:
+                    # calculate the centers of each marker
+                    corners = []
+                    for marker in markers:
+                        marker = marker.reshape(4, 2)
+                        corners.append(
+                            (
+                                marker[0][0] + (marker[2][0] - marker[0][0]) / 2,
+                                marker[0][1] + (marker[2][1] - marker[0][1]) / 2,
+                            )
+                        )
+
+                    # calculate the diagonal between min/max corner
+                    corners = np.array(corners)[: np.newaxis]
+                    corners_diagonal = math.hypot(
+                        np.max(np.max(corners, 0)) - np.min(np.min(corners, 0)),
+                        np.max(np.max(corners, 1)) - np.min(np.min(corners, 1)),
+                    )
+
+                    # if the drawing is close enough scan it
+                    if (corners_diagonal / diagonal) > 0.75:
+                        date = datetime.now().strftime("%Y%m%d%H%M%S%f")
+                        temp_file = os.path.join(self.temp, f"{date}.png")
+                        cv2.imwrite(temp_file, frame)
+                        image, template_id = self.scanner.scan(temp_file)
+                        if image is not None:
+                            out_file = os.path.join(self.outbound, f"{date}-{template_id}.png")
+                            cv2.imwrite(out_file, image)
+                            if self.callback:
+                                self.callback(out_file)
+                                self.close()
+                    else:
+                        show_come_closer = True
+                else:
+                    show_instructions = True
+
+                # draw frame in pygame
+                pg_frame = pygame.image.frombuffer(frame.tobytes(), frame.shape[1::-1], "BGR")
+                scale = min(self.size[0] / pg_frame.get_width(), self.size[1] / pg_frame.get_height())
+                self.frame = pygame.transform.scale_by(pg_frame, scale)
+                if show_instructions:
+
+                    self.frame.blit(pygame.transform.scale(self.instructions, self.frame.get_size()), (0, 0))
+                elif show_come_closer:
+                    self.frame.blit(pygame.transform.scale(self.closer, self.frame.get_size()), (0, 0))
+
+                self.last_frame_ts = time.time()
+
+            else:
+                # show black screen if we've got no image from camera
+                self.frame = pygame.Surface(self.size)
+                self.frame.fill((0, 0, 0, 0))
+
+    def render(self, surface):
+        if self.frame:
+            surface.blit(self.frame, self.pos)
